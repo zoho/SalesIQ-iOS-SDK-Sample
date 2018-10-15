1c80a07402bb9e79aa0bd54405207e71f5048078
Madhusudhan Rao <madhusudhan.rao@zohocorp.com>
2018-10-15 12:42:59 +0530

    *Removed Mobilisten.framework and added pod file

--- SalesIQiOSSDKDemo/AppDelegate.swift
+++ SalesIQiOSSDKDemo/AppDelegate.swift
@@ -10,13 +10,13 @@ import UIKit
 import Mobilisten
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-      
+        
         //Generate your own appkey and acceskey with your bundleidentifier
         ZohoSalesIQ.initWithAppKey("", accessKey: "")
         
@@ -25,8 +25,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         ZohoSalesIQ.Visitor.setEmail("")
         ZohoSalesIQ.Visitor.setContactNumber("")
         ZohoSalesIQ.Visitor.addInfo("", value:"")//ZohoSalesIQ.Visitor.addInfo("User type", value:"Premium")
-        ZohoSalesIQ.visitorId = "" //unique key to identify user and save conversation
-       
+        
+        ZohoSalesIQ.registerVisitor("")  //unique key to identify user and save conversation
         
         //Chat Customization
         
@@ -36,41 +36,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         ZohoSalesIQ.Chat.setVisibility(.agent, visible:true) // To show operator image in chat transcript
         ZohoSalesIQ.Chat.setTitle("Chat with us!"); //To set chat title
         
-        
         //Set theme color
         ZohoSalesIQ.Chat.setThemeColor(UIColor.green)
-
-        //Conversation
-        ZohoSalesIQ.Chat.setVisibility(.conversations, visible:true) //To show conversations
-        
-        //FAQ
-        ZohoSalesIQ.Chat.setVisibility(.faq, visible:true) //To show conversations
-        
         
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
 }
 
